34619b3844c7edd9c9135b0956010f05c39e0f7f
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-06 18:07:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(int)
+  test_array = (2..9).to_a
+  test_array.each do |test|
+    if int < 0
+      return false
+    elsif int == test
+      
+    elsif int % test === 0
+      return false
+    else
+      return true
+    end
+  end
+end
